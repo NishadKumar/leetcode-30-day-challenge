1283a0832001c5884a1d51cb877d7a63947a75d5
Nishad Vijayakumar <nishadkumar90@gmail.com>
2020-05-03 12:44:07 -0500

    Add files via upload

---
+++ first-unique-number.py
@@ -0,0 +1,73 @@
+You have a queue of integers, you need to retrieve the first unique integer in the queue.
+
+Implement the FirstUnique class:
+
+FirstUnique(int[] nums) Initializes the object with the numbers in the queue.
+int showFirstUnique() returns the value of the first unique integer of the queue, and returns -1 if there is no such integer.
+void add(int value) insert value to the queue.
+ 
+
+Example 1:
+
+Input: 
+["FirstUnique","showFirstUnique","add","showFirstUnique","add","showFirstUnique","add","showFirstUnique"]
+[[[2,3,5]],[],[5],[],[2],[],[3],[]]
+Output: 
+[null,2,null,2,null,3,null,-1]
+Explanation: 
+FirstUnique firstUnique = new FirstUnique([2,3,5]);
+firstUnique.showFirstUnique(); // return 2
+firstUnique.add(5);            // the queue is now [2,3,5,5]
+firstUnique.showFirstUnique(); // return 2
+firstUnique.add(2);            // the queue is now [2,3,5,5,2]
+firstUnique.showFirstUnique(); // return 3
+firstUnique.add(3);            // the queue is now [2,3,5,5,2,3]
+firstUnique.showFirstUnique(); // return -1
+
+class FirstUnique(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.deque = collections.deque()
+        self.lookup = {}
+        
+        for num in nums:
+            self.add(num)
+        
+
+    def showFirstUnique(self):
+        """
+        :rtype: int
+        """
+        if not self.deque:
+            return -1
+        
+        while self.deque and self.lookup[self.deque[0]] >= 2:
+            self.deque.popleft()
+        
+        if not self.deque:
+            return -1
+        
+        return self.deque[0]
+        
+
+    def add(self, value):
+        """
+        :type value: int
+        :rtype: None
+        """
+        if value in self.lookup:
+            self.lookup[value] += 1
+        else:
+            self.lookup[value] = 1
+        
+        self.deque.append(value)
+        
+
+
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
